75c2b97ce44272f4b66935fc510dad91f9bc2534
Alunos <eduardacristinasilva2008@gmail.com>
2024-07-22 17:19:44 -0300

    ff

---
+++ attRevisao1/Carta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attRevisao1
+{
+    public class Carta
+    {
+        public string Nipe { get; set; }
+        public string Tipo { get; set; }
+        public int Numero { get; set; }
+
+        public Carta(string nipe, string tipo, int numero)
+        {
+            Nipe = nipe;
+            Tipo = tipo;
+            Numero = numero;
+        }
+
+       public void ExibirDetalherCarta()
+        {
+            Console.WriteLine($"Nipe da Carta: {Nipe}" +
+                              $"\nTipo da Carta: {Tipo}" +
+                              $"\nNúmero da Carta: {Numero}");
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
---
+++ attRevisao1/Program.cs
@@ -0,0 +1,7 @@
+﻿using attRevisao1;
+using Revisao2;
+
+Carta c = new Carta("Espadas","",6);
+Carta c1 = new Carta("Espadas", "Rei", 0);
+c.ExibirDetalherCarta();
+c1.ExibirDetalherCarta();
